77c88cc0cc0dae169332b1ac0b29185a45c6ef3a
Sahar Sheikholeslami <sahara_sheik@hotmail.com>
2021-07-20 19:02:23 -0400

    scene builder for main page done / methods defined

--- src/main/java/ucf/assignments/InventoryListPageController.java
+++ src/main/java/ucf/assignments/InventoryListPageController.java
@@ -5,5 +5,21 @@ package ucf.assignments;
  *  Copyright 2021 first_name last_name
  */
 
+import javafx.scene.input.MouseEvent;
+
 public class InventoryListPageController {
+    public void addItem(MouseEvent mouseEvent) {
+    }
+
+    public void searchItem(MouseEvent mouseEvent) {
+    }
+
+    public void deleteItem(MouseEvent mouseEvent) {
+    }
+
+    public void editItem(MouseEvent mouseEvent) {
+    }
+
+    public void saveItems(MouseEvent mouseEvent) {
+    }
 }
