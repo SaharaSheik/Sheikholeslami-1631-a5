a778cc6bc6b10d51e70c5f1f055c5ef0a3f7eea0
Sahar Sheikholeslami <sahara_sheik@hotmail.com>
2021-07-20 19:30:26 -0400

    main done

--- src/main/java/ucf/assignments/InventoryTrackerMain.java
+++ src/main/java/ucf/assignments/InventoryTrackerMain.java
@@ -6,7 +6,36 @@ package ucf.assignments;
  */
 
 
-public class InventoryTrackerMain {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
+public class InventoryTrackerMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InventoryListPage.fxml")));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Inventory Tracker");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
